78e6c6af46016365af4d87dd7dde60e4e5e57e48
Sergey Kolomenkin <sergey@pcspeedup.com>
2015-05-05 18:24:06 +0300

    adding curlpp project

--- src/common/Common.cpp
+++ src/common/Common.cpp
@@ -7,6 +7,7 @@
 #if _MSC_VER > 1000
 #pragma comment (lib, "jsoncpp.lib")
 #pragma comment (lib, "libcurl.lib")
+#pragma comment (lib, "libcurlpp.lib")
 #endif // _MSC_VER > 1000
 
 //------------------------------------------------------
--- src/dping/main.cpp
+++ src/dping/main.cpp
@@ -2,8 +2,6 @@
 
 #include "stdafx.h"
 
-//#include <json\json.h>
-
 #include <iostream>
 
 using namespace std;
@@ -12,54 +10,8 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-#if 0
-	const string config_doc = R"zzz(
-// Configuration options
-{
-    // Default encoding for text
-    "encoding" : "UTF-8",
-    
-    // Plug-ins loaded at start-up
-    "plug-ins" : [
-        "python",
-        "c++",
-        "ruby"
-        ],
-        
-    // Tab indent size
-    "indent" : { "length" : 3, "use_space": true }
-}
-)zzz";
+	cout << "Hello, world!" << endl;
 
-	Json::Value root;   // will contains the root value after parsing.
-	Json::Reader reader;
-	bool parsingSuccessful = reader.parse(config_doc, root);
-	if (!parsingSuccessful)
-	{
-		// report to the user the failure and their locations in the document.
-		cout << "Failed to parse configuration\n"
-			<< reader.getFormattedErrorMessages();
-		return 1;
-	}
-
-	// Get the value of the member of root named 'encoding', return 'UTF-8' if there is no
-	// such member.
-	const string encoding = root.get("encoding", "UTF-8").asString();
-	// Get the value of the member of root named 'encoding', return a 'null' value if
-	// there is no such member.
-	const Json::Value plugins = root["plug-ins"];
-	for (size_t index = 0; index < plugins.size(); ++index)  // Iterates over the sequence elements.
-	{
-		cout << "plugin: " << plugins[index].asString() << endl;
-	}
-
-	Json::StyledWriter writer;
-	// Make a new JSON document for the configuration. Preserve original comments.
-	const string outputConfig = writer.write(root);
-
-	// And you can write to a stream, using the StyledWriter automatically.
-	cout << root["plug-ins"] << endl;
-#endif
 	return 0;
 }
 
