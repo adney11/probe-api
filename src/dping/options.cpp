1004417de07070ea9c14e5b04a67d61417ae5d5d
Sergey Kolomenkin <sergey@pcspeedup.com>
2015-05-18 15:50:39 +0300

    changed "ProbeAPI tools" to "ProbeAPI Tools"

--- src/dping/options.cpp
+++ src/dping/options.cpp
@@ -149,7 +149,7 @@ string GetPrintCredits()
 
 The MIT License (MIT)
 
-Copyright (c) 2015 ProbeAPI tools
+Copyright (c) 2015 ProbeAPI Tools
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
