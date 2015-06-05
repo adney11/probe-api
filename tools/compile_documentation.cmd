7af10ecba89580496b577f07d84e8cde649f8ba2
Sergey Kolomenkin <sergey@pcspeedup.com>
2015-06-05 22:16:08 +0300

    writing new documentation generator

--- tools/compile_documentation.cmd
+++ tools/compile_documentation.cmd
@@ -1,11 +1,7 @@
 @echo off
 
-set parts="%~dp0text_parts"
-set dest="%~dp0.."
-set bin="%~dp0..\Release"
-
-set strip_first_line=awk "{ a[i++] = $0 } END { for (j=1; j<i; j++) print a[j] }"
-set strip_first_two_lines=awk "{ a[i++] = $0 } END { for (j=2; j<i; j++) print a[j] }"
+set path=%~dp0..\Release;%PATH%
+pushd "%~dp0"
 
 echo Rebuilding documentation...
 echo Note! You need the following unix utils available via PATH variable:
@@ -15,69 +11,27 @@ echo.
 
 REM ======================================================
 echo README.md...
-set destfile=%dest%\README.md
 
-copy %parts%\README_10.md %destfile%
-%bin%\dping.exe --version | %strip_first_line% >> %destfile%
-type %parts%\README_100.md >> %destfile%
+awk -f .\file-formatter.awk README.md.in > ..\README2.md
 
 REM ======================================================
-echo dping.windows.md...
-set tool_name=dping
-set destfile=%dest%\doc\%tool_name%.windows.md
-set tool=%bin%\%tool_name%.exe
-set part_pref=%parts%\%tool_name%
 
-copy %part_pref%_10.md %destfile%
-%tool% --help | %strip_first_two_lines% >> %destfile%
-type %part_pref%_20.md >> %destfile%
-call :print_with_middle_trim "--list-country" 10 2 1
-type %part_pref%_30.md >> %destfile%
-call :print_with_middle_trim "--list-asn ES" 10 2 1
-type %part_pref%_40.md >> %destfile%
-call :print_with_middle_trim "--country US 8.8.8.8" 10 7 1
-type %part_pref%_50.md >> %destfile%
-call :print_with_middle_trim "--asn AS3352 8.8.8.8" 10 7 1
-type %part_pref%_60.md >> %destfile%
+echo doc/dping.windows.md...
+
+awk -f .\file-formatter.awk dping.md.in > ../doc/dping.windows.md
 
 REM ======================================================
-echo dtraceroute.windows.md...
-set tool_name=dtracert
-set destfile=%dest%\doc\dtraceroute.windows.md
-set tool=%bin%\%tool_name%.exe
-set part_pref=%parts%\dtraceroute
 
-copy %part_pref%_10.md %destfile%
-%tool% --help | %strip_first_two_lines% >> %destfile%
-type %part_pref%_20.md >> %destfile%
-call :print_with_middle_trim "--list-country" 10 2 1
-type %part_pref%_30.md >> %destfile%
-call :print_with_middle_trim "--list-asn ES" 10 2 1
-type %part_pref%_40.md >> %destfile%
-call :print_with_middle_trim "--country US 8.8.8.8" 20 14 3
-type %part_pref%_50.md >> %destfile%
-call :print_with_middle_trim "--asn AS3320 8.8.8.8" 20 14 3
-type %part_pref%_60.md >> %destfile%
+echo doc/dtraceroute.windows.md...
+
+awk -f .\file-formatter.awk dtraceroute.md.in > ../doc/dtraceroute.windows.md
 
 REM ======================================================
 
 echo Done!
+popd
 pause
 
-goto :eof
-
-REM ======================================================
-:print_with_middle_trim
-REM usage:
-REM call :print_with_middle_trim "arguments for tool" [keep_first_N] [keep_last_N] [insert_N]
-set args=%~1
-set first=%~2
-set last=%~3
-set insert=%~4
-
-echo %tool_name% %args%...
-echo $ %tool_name% %args%>> %destfile%
-%tool% %args% | awk "{ a[i++] = $0 } END { b=1; for (j=0; j<i; j++) if(j < %first% || j >= i-%last%) print a[j]; else if(b>0) { for(k=0; k<%insert%; k++) print \"...\"; b=0; } }" >> %destfile%
 goto :eof
 
 REM ======================================================
--- tools/text_parts/dtraceroute_10.md
+++
@@ -1,8 +0,0 @@
-# dtraceroute
-
-## About
-`dtraceroute` is a tool which works almost the same as Windows and Linux built-in tool, but all network operations are going from different hosts in internet.
-
-## Usage
-
-```
